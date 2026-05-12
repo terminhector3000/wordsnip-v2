ad4b7d4b89e0932173aea56131b7b829698792d3
Hector Tapias <hectortapias@Hectors-MacBook-Air-2.local>
2026-05-12 09:21:10 -0700

    creates logo for wordsnip, install and configures tailwindcss

--- frontend/wordsnip/src/pages/home.tsx
+++ frontend/wordsnip/src/pages/home.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import type { SnipEngine } from "../types/WordsnipType";
+import WordSnipLogo from "../components/wsLogo";
 import WordsnipForm from "../components/worsnipForm";
 import RenderEngineResult from "../components/renderEngineResult";
 
@@ -13,9 +14,20 @@ const Home = () => {
 
   return (
     <div>
-      <Link to="/contact">Contact</Link>
-      <WordsnipForm onSuccessfulSubmit={onSuccessfulSubmit} />
-      {snipData ? <RenderEngineResult data={snipData} /> : "waiting for input"}
+      <div className="bg-slate-50">
+        <div className="w-40 md:w-48 lg:w-56 py-8">
+          <WordSnipLogo className="h-10 md:h-12 w-auto transition-transform duration-200 hover:scale-105 dark:invert" />
+        </div>
+      </div>
+      <div>
+        <Link to="/contact">Contact</Link>
+        <WordsnipForm onSuccessfulSubmit={onSuccessfulSubmit} />
+        {snipData ? (
+          <RenderEngineResult data={snipData} />
+        ) : (
+          "waiting for input"
+        )}
+      </div>
     </div>
   );
 };
